622ddd02477a8eecb6ff8301b6f4c620bf533af6
TangfeiTM <iamtangfei@gmail.com>
2014-08-07 09:19:35 +0800

    add sql script

---
+++ sql-design/ContactBackup_Server.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE contactsbak;
+
+USE contactsbak;
+
+CREATE TABLE users(
+	userid integer(10) NOT NULL AUTO_INCREMENT,
+	accountname varchar(320) NOT NULL,
+	password varchar(20) NOT NULL,
+	PRIMARY KEY (userid)
+);
+
+CREATE TABLE contacts(
+  userid integer(10) NOT NULL,
+  name varchar(50),
+  phone_1 varchar(15),
+  phone_2 varchar(15),
+  phone_3 varchar(15),
+  phone_4 varchar(15),
+  email_1 varchar(320),
+  email_2 varchar(320),
+  address_1 varchar(100),
+  address_2 varchar(100),
+  birthday date,
+  organization varchar(320),
+  data_1 varchar(50),
+  data_2 varchar(50),
+  data_3 varchar(50),
+  FOREIGN KEY (userid) REFERENCES users(userid)
+);
